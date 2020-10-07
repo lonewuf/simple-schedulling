19c0dc28efb6e8912c6e618f8d0b44e08311d243
tawbuts <john.nico.austria@gmail.com>
2020-10-07 08:33:09 -0700

    added code in server to show the frontend

--- server.js
+++ server.js
@@ -40,6 +40,17 @@ app.use(passport.session());
 app.use('/schedule', scheduleRoutes);
 app.use('/user', userRoutes);
 
+if (process.env.NODE_ENV === 'production') {
+	// set static folder
+	app.use(express.static('client/build'));
+
+	app.get('*', (req, res) => {
+		res.sendFile(path.resolve(__dirname, 'client', 'build'));
+	});
+}
+
 const port = process.env.PORT || 5000;
-// const host = process.env.HOST || '0.0.0.0';
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const host = process.env.HOST || '0.0.0.0';
+app.listen(port, host, () =>
+	console.log(`Listening on port ${port}, host ${host}`)
+);
